5278041014bbda3ec7845b82b2f6119bc5764288
eduardolmsj <dudump10111@usp.br>
2023-10-24 16:02:16 -0300

    .

---
+++ lc43_adc/Debug/source/HL_errata_SSWF021_45.d
@@ -0,0 +1,12 @@
+# FIXED
+
+source/HL_errata_SSWF021_45.obj: ../source/HL_errata_SSWF021_45.c
+source/HL_errata_SSWF021_45.obj: C:/Users/Eduardo/workspace_v12/lc43_adc/include/HL_errata_SSWF021_45_defs.h
+source/HL_errata_SSWF021_45.obj: C:/Users/Eduardo/workspace_v12/lc43_adc/include/HL_errata_SSWF021_45.h
+
+../source/HL_errata_SSWF021_45.c:
+
+C:/Users/Eduardo/workspace_v12/lc43_adc/include/HL_errata_SSWF021_45_defs.h:
+
+C:/Users/Eduardo/workspace_v12/lc43_adc/include/HL_errata_SSWF021_45.h:
+
